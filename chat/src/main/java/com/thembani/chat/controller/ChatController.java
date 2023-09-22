3d13ef72ae12a13c47de3c7411379bfc1062414f
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-09-22 10:34:45 +0200

    Update ChatController.java

--- chat/src/main/java/com/thembani/chat/controller/ChatController.java
+++ chat/src/main/java/com/thembani/chat/controller/ChatController.java
@@ -1,8 +1,24 @@
 package com.thembani.chat.controller;
 
+import com.thembani.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+    @MessageMapping("/chat.saveUser")
+    @SendTo("/topic/public")
+    public ChatMessage saveUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
 }
