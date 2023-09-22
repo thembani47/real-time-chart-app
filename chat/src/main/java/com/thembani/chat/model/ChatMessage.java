b7c09fae2400c4d3223e79f051cd33aa458a9ae0
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2023-09-22 10:35:03 +0200

    Create ChatMessage.java

---
+++ chat/src/main/java/com/thembani/chat/model/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.thembani.chat.model;
+
+import lombok.*;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
